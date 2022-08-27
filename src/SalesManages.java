6ee9c8cd5dfb8a985fa47cd7ddb8d7b483fe4d35
IgorBoyarincev <boyarincev.igor@list.ru>
2022-08-27 13:28:04 +0300

    все типа long

--- src/SalesManages.java
+++ src/SalesManages.java
@@ -1,13 +1,13 @@
 public class SalesManages {
-    protected int[] sales;
+    protected long[] sales;
 
-    public SalesManages(int[] sales) {
+    public SalesManages(long[] sales) {
         this.sales = sales;
     }
 
-    public int max() {
-        int max = -1;
-        for (int sale : sales) {
+    public long max() {
+        long max = -1;
+        for (long sale : sales) {
             if (sale > max) {
                 max = sale;
             }
